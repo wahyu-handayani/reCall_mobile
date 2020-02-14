41d10f190d9222897f442817c2b930fe103371c4
wahyu-handayani <handayaniwahyu093@gmail.com>
2020-02-14 11:43:01 +0700

    add lifecycle componentDidUpdate

--- src/Company.js
+++ src/Company.js
@@ -10,12 +10,41 @@ export default class Company extends Component {
     this.state = {
       users: [],
       search: '',
+      searchh: '',
       isFetching: false,
     }
   }
   shouldComponentUpdate(a, b, c) {
     console.log('sdfdaf', a, b, c)
     return true
+  }
+  onSearchh = (key, val) => {
+    this.setState({ [key]: val })
+  }
+  async componentDidUpdate(_, prevState) {
+    const token = await AsyncStorage.getItem('token')
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+    console.log(this.state.searchh, 'this.state.searchh')
+    console.log(prevState, 'prevState')
+    if (this.state.searchh !== prevState.search) {
+      axios.get(`http://192.168.100.3:4000/search?search=${this.state.searchh}`, config)
+        .then(result => {
+          if (result.data.data == undefined) {
+            this.setState({
+              users: ''
+            })
+          } else {
+            this.setState({
+              users: result.data.data
+            })
+          }
+        })
+        .catch(err => console.log(err))
+    } else console.log('SAMA')
   }
   onSearch = async (key, val) => {
     const token = await AsyncStorage.getItem('token')
@@ -74,6 +103,10 @@ export default class Company extends Component {
           style={{ borderColor: 'red', height: 70, backgroundColor: 'transparent', width: 360, fontSize: 18 }}
           placeholder='Search...'
           onChangeText={val => this.onSearch('search', val)} />
+        <TextInput
+          style={{ borderColor: 'red', height: 70, backgroundColor: 'pink', width: 360, fontSize: 18 }}
+          placeholder='Search2...'
+          onChangeText={val => this.onSearchh('searchh', val)} />
         <FlatList
           data={this.state.users}
           showsVerticalScrollIndicator={false}
@@ -94,7 +127,7 @@ export default class Company extends Component {
                   </Body>
                 </CardItem>
               </Card> */}
-              <MyCard name={item.name} skill={item.skill}/>
+              <MyCard name={item.name} skill={item.skill} />
             </View>
           }
           keyExtractor={item => item.email}
